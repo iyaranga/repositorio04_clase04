fb9d9a06beaf657c03e08b79f29f4516523c9fca
ipyv <ipyv@hp>
2025-06-09 21:20:56 -0500

    carga inicial

--- src/programas/Prueba.java
+++ src/programas/Prueba.java
@@ -11,5 +11,6 @@ package programas;
 public class Prueba {
     public static void main(String[] args){
         System.out.println("Clase Prueba");
+        System.out.println("Clase Prueba2");
     }
 }
